18d984bbd40d947cf00640b61c51893715858f3f
SpartificialUdemy <instructors@spartificial.com>
2024-11-16 14:52:34 +0530

    converted API endpoint paths

--- frontend.py
+++ frontend.py
@@ -30,8 +30,8 @@ st.markdown(
 st.markdown('<h1 style="color: lightgray;">🌙 Lunar Terrain Segmentation App</h1>', unsafe_allow_html=True)
 
 # FastAPI backend endpoints
-PREPROCESS_URL = "https://project-3-ik42.onrender.com/preprocess/"
-SEGMENT_URL = "https://project-3-ik42.onrender.com/segment/"
+PREPROCESS_URL = "http://localhost:8000/preprocess/"
+SEGMENT_URL = "http://localhost:8000/segment/"
 
 # Initialize session state for storing images
 if 'preprocessed_image' not in st.session_state:
